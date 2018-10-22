a3fa5aa83cb7975c269b11a04b419d124f31ba15
Christophe <cdeguillage@free.fr>
2018-10-22 16:31:48 +0200

    products alias count

--- docs/sql/products.sql
+++ docs/sql/products.sql
@@ -3,6 +3,6 @@ SELECT p.* FROM pizzastore.pizza p WHERE p.price < 10;
 SELECT p.* FROM pizzastore.pizza p WHERE p.image IS NULL;
 SELECT p.* FROM pizzastore.pizza p ORDER BY p.price DESC;
 SELECT p.* FROM pizzastore.pizza p ORDER BY RAND() LIMIT 3;
-SELECT COUNT(1) FROM pizzastore.pizza p;
+SELECT COUNT(1) AS nb_pizza FROM pizzastore.pizza p;
 SELECT p.* FROM pizzastore.pizza p ORDER BY p.price DESC LIMIT 1;
 SELECT AVG(p.price) FROM pizzastore.pizza p;
