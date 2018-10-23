decdd0b3afa7d25d2681f3aef81dada078ecb42b
Christophe <cdeguillage@free.fr>
2018-10-23 09:25:12 +0200

    update products.sql

--- docs/sql/products.sql
+++ docs/sql/products.sql
@@ -1,8 +1,15 @@
-SELECT p.* FROM pizzastore.pizza p;
-SELECT p.* FROM pizzastore.pizza p WHERE p.price < 10;
-SELECT p.* FROM pizzastore.pizza p WHERE p.image IS NULL;
-SELECT p.* FROM pizzastore.pizza p ORDER BY p.price DESC;
-SELECT p.* FROM pizzastore.pizza p ORDER BY RAND() LIMIT 3;
-SELECT COUNT(1) AS nb_pizza FROM pizzastore.pizza p;
-SELECT p.* FROM pizzastore.pizza p ORDER BY p.price DESC LIMIT 1;
-SELECT AVG(p.price) FROM pizzastore.pizza p;
+USE pizzastore;
+SELECT p.* FROM pizza p;
+SELECT p.* FROM pizza p WHERE p.price < 10;
+SELECT p.* FROM pizza p WHERE p.image IS NULL;
+SELECT p.* FROM pizza p ORDER BY p.price DESC;
+SELECT p.* FROM pizza p ORDER BY RAND() LIMIT 3;
+/* CREATE INDEX pizza_price_IDX ON pizza (price); */
+SELECT COUNT(1) AS nb_pizza FROM pizza p;
+SELECT p.* FROM pizza p ORDER BY p.price DESC LIMIT 1;
+SELECT p.* FROM pizza p WHERE p.price IN (SELECT MAX(sp.price) FROM pizza sp);
+SELECT p.* FROM pizza p WHERE p.price = (SELECT MAX(sp.price) FROM pizza sp);
+
+SELECT AVG(p.price) FROM pizza p;
+
+
